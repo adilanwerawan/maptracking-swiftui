ad63e8b816aec4b15b85cfb88014881e1936194c
Adil Anwer <adil@cognitivehealthintl.com>
2021-04-25 08:18:52 +0500

    Initial Commit

---
+++ assign4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  assign4
+//
+//  Created by Adil Anwer on 25/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ assign4/assign4App.swift
@@ -0,0 +1,17 @@
+//
+//  assign4App.swift
+//  assign4
+//
+//  Created by Adil Anwer on 25/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct assign4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
